7aa348f6dac5ddef82587f75cb084db44f8bc986
Kevin Santacruz <--global>
2019-02-19 16:50:44 -0500

    envio de notificacion via twitter (datos quemados)

--- RecieveEPHClient/SimpleEventProcessor.cs
+++ RecieveEPHClient/SimpleEventProcessor.cs
@@ -20,6 +20,8 @@ namespace RecieveEPHClient
     {
         private HttpClient client;
         private SmartButtonContext db;
+        public string direct_messages_uri = "https://api.twitter.com/1.1/direct_messages/events/new.json";
+
         public Task CloseAsync(PartitionContext context, CloseReason reason)
         {
             Console.WriteLine($"Processor Shutting Down. Partition '{context.PartitionId}', Reason: '{reason}'.");
@@ -45,6 +47,7 @@ namespace RecieveEPHClient
                 //Logica de negocio
                 string data = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
 
+<<<<<<< HEAD
                 Console.WriteLine(data);
 
                 SmartButton sm = JsonConvert.DeserializeObject<SmartButton>(data);
@@ -78,10 +81,26 @@ namespace RecieveEPHClient
                 }
                 EventProcessor.events += 1;
                 Console.WriteLine($"Procesando. Particion: {context.PartitionId} {sm.Data} .Estado: {sm.Status}, Id {sm.DeviceId}, lat: {sm.Latitude}, longitude: {sm.Longitude}");
+=======
+                // TODO
+                // Consultar datos de la configuracion del boton
+
+                string userIdRC = "17747093";
+                string userIdKS = "3392698503";
+                string userIdJF = "1084864679757926401";
+
+                string temp = "Hola tu id de twitter es:";
+
+                SendNotification(userIdRC, $"{temp} {userIdRC}");
+                SendNotification(userIdKS, $"{temp} {userIdKS}");
+                SendNotification(userIdJF, $"{temp} {userIdJF}");
+
+>>>>>>> envio de notificacion via twitter (datos quemados)
             }
             return context.CheckpointAsync();
         }
 
+<<<<<<< HEAD
         private UserDevices getDevice(string Id)
         {
             db = new SmartButtonContext();
@@ -126,5 +145,84 @@ namespace RecieveEPHClient
             }
 
         }
+=======
+        /// <summary>
+        /// Send notification twitter's via
+        /// </summary>
+        /// <param name="userId">Recipient's id</param>
+        /// <param name="Message">Message body</param>
+        /// <returns></returns>
+        private async Task<bool> SendNotification(string userId, string Message)
+        {
+            client = new HttpClient();
+
+            string AuthHeader = ProjectComponent.GenerateTwitterAuthHeader(direct_messages_uri); client.DefaultRequestHeaders.Add("Authorization", AuthHeader); client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string json = ProjectComponent.ParseJson(userId, Message);
+            var response = client.PostAsync(direct_messages_uri, new StringContent(json, Encoding.UTF8, "application/json")).GetAwaiter().GetResult();
+            
+            Console.WriteLine($"It was send: {response.IsSuccessStatusCode}, userId: {userId}, message: {Message}");
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<string> SendReplyResponse(string Text, string ReplyToStatusId, string ReplyToUserId)
+        {
+            OAuthInfo OAI = new OAuthInfo()
+            {
+                ConsumerKey = Properties.Resources.TwConsumerKey,
+                ConsumerSecret = Properties.Resources.TwConsumerSecret,
+                AccessToken = Properties.Resources.TwToken,
+                AccessSecret = Properties.Resources.TwTokenSecret
+            };
+
+            TinyTwitter TT = new TinyTwitter(OAI);
+            TT.ReplyStatus(Text, ReplyToStatusId, ReplyToUserId);
+            return "OK";
+        }
+
+        public async Task<bool> CheckFriendship(string BotScreenName, string RecipientUsername)
+        {
+            bool IsFriend = false;
+            OAuthInfo OAI = new OAuthInfo()
+            {
+                ConsumerKey = Properties.Resources.TwConsumerKey,
+                ConsumerSecret = Properties.Resources.TwConsumerSecret,
+                AccessToken = Properties.Resources.TwToken,
+                AccessSecret = Properties.Resources.TwTokenSecret
+            };
+
+            TinyTwitter TT = new TinyTwitter(OAI);
+
+            string CheckFriendJson = TT.CheckFriendship(BotScreenName, RecipientUsername);
+
+            CheckFriendship CF = (CheckFriendship)Newtonsoft.Json.JsonConvert.DeserializeObject(CheckFriendJson, typeof(CheckFriendship));
+
+            if (CF != null)
+
+            {
+                IsFriend = CF.Relationship.Source.Following;
+
+            }
+            return IsFriend;
+        }
+
+        private async Task<string> FollowUserId(string UserId)
+        {
+            bool IsFriend = false;
+            OAuthInfo OAI = new OAuthInfo()
+            {
+                ConsumerKey = Properties.Resources.TwConsumerKey,
+                ConsumerSecret = Properties.Resources.TwConsumerSecret,
+                AccessToken = Properties.Resources.TwToken,
+                AccessSecret = Properties.Resources.TwTokenSecret
+            };
+            TinyTwitter TT = new TinyTwitter(OAI);
+            TT.FollowUserId(UserId);
+            return "OK";
+
+        }
+
+>>>>>>> envio de notificacion via twitter (datos quemados)
     }
 }
